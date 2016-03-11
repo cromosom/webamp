f0f3bd78751fd3c98b86c62a487554a3119ec49b
tim <t.woldt@agentur-sowhat.de>
2016-03-11 12:55:17 +0100

    Cleanup index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 require('./styles.css');
 
-var moduleStack = [];
-
 var playlist = require('./components/playlist.js');
 playlist.init();
-//console.log(playlist.items);
+
 var audio = require('./components/audio.js');
 audio.init(playlist.items);
 
